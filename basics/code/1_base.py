b4159e19d1ee813e50e132b22147cb24c9a4bfec
wayne <waynebeckom@gmail.com>
2026-01-20 22:47:03 -0700

    textures loaded

--- basics/code/1_base.py
+++ basics/code/1_base.py
@@ -1,12 +1,26 @@
 from pyray import *
 from raylib import *
+from os.path import join 
+
 
 init_window(1920, 1080, "Base")
+
+
+
+# import you must import it once before the game function
+spaceship_texture = load_texture('../assets/spaceship.png') #for some reason you have to manuever and find the spaceship??? 
+spaceship_image = load_image('../assets/spaceship.png') 
+image_color_grayscale(spaceship_image) 
+new_texture = load_texture_from_image(spaceship_image) 
+
+
 
 
 while not window_should_close():
     begin_drawing()
     clear_background(BLACK)
+
+    # custom shape drawing
     draw_pixel(100, 200, RED)
     draw_pixel_v(Vector2(200, 200), WHITE)
 
@@ -15,10 +29,11 @@ while not window_should_close():
 
     draw_line_ex(Vector2(0, 0), Vector2(500, 200), 10, RED)
 
-    # YOUTUBE TIME STAMP 20:19 https://www.youtube.com/watch?v=UoAsDlUwjy0
+    # YOUTUBE TIME STAMP 31:00 https://www.youtube.com/watch?v=UoAsDlUwjy0
 
-
-
+    # display images
+    draw_texture(spaceship_texture, 0, 0, WHITE)
+    draw_texture_v(new_texture, Vector2(100,0),WHITE)
     end_drawing()
 
 close_window()
